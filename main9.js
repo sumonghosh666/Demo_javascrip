50edba62f585844ae26c02bd68a0ed2dc6187494
sumonghosh666 <sumonghosh666@gmail.com>
2021-10-29 20:55:06 +0600

    new practice

---
+++ main9.js
@@ -0,0 +1,15 @@
+// Node js URL Module
+// Till now not completed
+
+var http=require('http');
+var URL=require('url')
+
+var server=http.createServer(function(req,res) {
+
+  
+
+
+}) ;
+
+server.listen(5050);
+console.log("Server Run Success ");
